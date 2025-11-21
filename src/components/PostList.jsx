5e2c60990f1ba88a50af3bf8e56eb78a6c8fb58a
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-11-22 02:22:54 +0630

    Submit and cancel button for new post are added

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,31 +5,14 @@ import { useState } from "react";
 import Modal from "./Modal";
 
 const PostList = ({ modalIsVisible, hideModalHandler }) => {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {modalIsVisible && (
         <Modal hideModalHandler={hideModalHandler}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            enteredBody={enteredBody}
-            enteredAuthor={enteredAuthor}
-          />
+          <NewPost onCancel={hideModalHandler} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} text={enteredBody} />
         <Post author="SRT" text="Text2" />
       </ul>
     </>

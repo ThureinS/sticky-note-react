09fd10d9b2b34898778537b5dcc8282459d83fe7
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-11-25 09:39:43 +0630

    post fetch and loading state

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,38 +1,57 @@
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import Modal from "./Modal";
 
-const PostList = ({ modalIsVisible, hideModalHandler }) => {
-  const [posts, setPosts] = useState([]);
+const PostList = ({modalIsVisible, hideModalHandler}) => {
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  function addPostHandler(postData) {
-    setPosts((prevPosts) => [postData, ...prevPosts]);
-  }
-  return (
-    <>
-      {modalIsVisible && (
-        <Modal hideModalHandler={hideModalHandler}>
-          <NewPost onCancel={hideModalHandler} onAddPost={addPostHandler} />
-        </Modal>
-      )}
+    useEffect(() => {
+        async function fetchPosts() {
+            const response = await fetch('http://localhost:8080/posts');
+            const postData = await response.json();
+            setPosts(postData.posts);
+        }
+        fetchPosts();
+    }, []);
 
-      {posts.length > 0 && (
-        <ul className={classes.posts}>
-          {posts.map((post) => (
-            <Post key={post.body} body={post.body} author={post.author} />
-          ))}
-        </ul>
-      )}
-      {posts.length === 0 && (
-        <div style={{ textAlign: "center", color: "white" }}>
-          <h2>There are no notes yet.</h2>
-          <p>Start adding some!</p>
-        </div>
-      )}
-    </>
-  );
+    function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts', {
+            method: "POST",
+            body: JSON.stringify(postData),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        setPosts((prevPosts) => [postData, ...prevPosts]);
+    }
+
+    return (
+        <>
+            {modalIsVisible && (
+                <Modal hideModalHandler={hideModalHandler}>
+                    <NewPost onCancel={hideModalHandler} onAddPost={addPostHandler}/>
+                </Modal>
+            )}
+
+            {!isLoading && posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => (
+                        <Post key={post.body} body={post.body} author={post.author}/>
+                    ))}
+                </ul>
+            )}
+            {isLoading && posts.length === 0 && (
+                <div style={{textAlign: "center", color: "white"}}>
+                    <h2>There are no notes yet.</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
+            {isLoading && (<h1 style={{textAlign: "center", color: "white"}}>Loading...</h1>)}
+        </>
+    );
 };
 
 export default PostList;
